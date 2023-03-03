21034b565038627e45b6fddbf160a1ed484602ce
littlelionstudios <flavorformulas@gmail.com>
2023-03-03 13:55:47 -0500

    please work

--- index.js
+++ index.js
@@ -33,32 +33,32 @@ let totalFame = 0;
 const gameOptions = {
     streamingPlatform: {
         text: 'Chose a Streaming Platform for VTubing',
-        image: './Game_Assets/twitch_youtube_tiktok.png',
+        image: './Assets/twitch_youtube_tiktok.png',
         explainer: 'Many VTubers stream across multiple platforms, including YouTube, Twitch, and even TikTok (to name a few), each with its own pros and cons. Twitch is the largest platform for live-streaming, with most of the content built around video games. Twitch also has a thriving community atmosphere. <br><br> Conversely, YouTube was made for uploading lasting content. It has a search engine that helps newer streamers find an audience, though it lacks a close-knit community. Finally, TikTok has a high conversion rate when converting your TikTok short-videos audience to your live stream audience, so building your audience on TikTok and later moving them to other platforms like YouTube/Twitch is a great strategy.',
         fame: 100,
         
     }, 
     vTuberModel: {
         text: `Create your VTuber Model`,
-        image: `./Game_Assets/PNG_2D_3D.png`,
+        image: `./Assets/PNG_2D_3D.png`,
         explainer: `There are three options to choose from when it comes to your VTuber avatar: PNG, 2D, or a 3D model. Whichever you choose will be a matter of personal preference and budget. You can make your own avatar using your own art skills and inexpensive/free programs ($), buy a pre-made model ($$), or commission someone to make you a custom avatar ($$$). <br><br> A PNG Model is a still image of your avatar. Many streamers use a PNG avatar when just starting out because it is the simplest and least expensive way of VTubing. However, PNG models are also the least engaging and entertaining because they can't move or have facial expressions. If you want to make your own PNG, you'll need to draw one in an image editing software. <br><br> A 2D VTuber avatar comprises flat, 2D art that has been "rigged" to move and is the most expensive option. Using a 2D model is one of the most popular choices for VTubing among VTubers because of their pleasing aesthetics (resembling anime characters), uniqueness (because they are highly customizable), and vibrant expressions. <br><br> 3D VTuber avatars are also trendy. A 3D VTuber model can be tracked from many angles because it is a three-dimensional object that is very immersive and engaging. There are several free options to make your own 3D model available. However, 2D and 3D models require extra face-tracking software to stream with them (e.g., VSeeFace, VTube Studio, etc.), unlike with PNGs. Again, different free and paid options exist for this, and availability is based on your computer's operating system (PC or Mac).`,
         fame: 100,
     },
     characterLore: {
         text: `Write your VTuber Character's Lore`,
-        image: `./Game_Assets/Gawr_Gura_Lore.png`,
+        image: `./Assets/Gawr_Gura_Lore.png`,
         explainer: `VTuber lore is your persona's backstory. While you don't need official lore, it can help make your character more interesting, personal, and unique and contribute to your channel's overall aesthetic and branding. Having a defined lore also helps some streamers get into character easier. Lore can include topics such as your character's likes and dislikes, goals and aspirations, race (for example, are they a part of the human race or another fantasy-based race like faeries or aliens), age (e.g., if they are an adult or child), and occupation (usually tied into their appearance/attire- like a persona who is royalty would dress in fancy/lavish clothing).`,
         fame: 100,
     }, 
     assetsOverlays: {
         text: `Create/Curate/Commission your VTuber Assets and Overlays`,
-        image: `./Game_Assets/VTuber_Assets_and_Overlays.png`,
+        image: `./Assets/VTuber_Assets_and_Overlays.png`,
         explainer: `Examples of VTuber assets include custom emotes for your channels; animated or static "Stream Starting," "Be Right Back, and "Streaming Ending" screens; background music (BGM); logos; and Twitch/YouTube alerts. Overlays are the main backdrops Vtubers use on their streaming software. There are different types of overlays, like minimalist overlays with simple elements and a clean theme, overlays that are detailed with several custom elements, or overlays with illustrative themes. While Twitch overlays are not required for live streaming, they can improve your viewers' experience and create engagement between yourself and your audience. <br><br> You can buy pre-made assets and overlays online (like on Etsy) or commission artists to create custom designs. There are tons of ways to get free Vtuber assets and overlays. You can also create your own using programs such as Canva.`,
         fame: 100,
     },
     socialMedia: {
         text: `Create a Social Media Presence`,
-        image: `./Game_Assets/Social_Media.png`,
+        image: `./Assets/Social_Media.png`,
         explainer: `Advertising your debut and engaging with your potential audience on Social Media is integral to VTubing. If the audience doesn't know you exist, how can they come to your streams? So create social media accounts on platforms like Instagram, TikTok, or Twitter (most of the VTuber community is very active on Twitter). Some ideas for pre-debut content include: <br>
         <ul>
         <li>Doing a partial character reveal of your VTuber model to tease your audience.</li>
@@ -69,19 +69,19 @@ const gameOptions = {
     },
     debutContent: {
         text: `Plan your Debut Content`,
-        image: `./Game_Assets/Plan_your_Debut.png`,
+        image: `./Assets/Plan_your_Debut.png`,
         explainer: `Come up with some ideas on what you will do on your VTuber debut. Most VTubers use their debut to showcase their models from foot to head, display their VTuber assets and overlays, and introduce themselves with details such as their character's height, age, race, likes and dislikes, lore, and fun facts. Try creating a rough schedule of your debut while allowing yourself time to answer questions and chatting with your audience.`,
         fame: 100,
     },
     vTuberCommunity: {
         text: `Build your VTuber Community`,
-        image: `./Game_Assets/Community.png`,
+        image: `./Assets/Community.png`,
         explainer: `As the saying goes, no one lives in a vacuum, and the same goes for VTubers. So start building your community and become friends with other VTubers by following and interacting with VTubers you like and admire. Interact with their tweets and also visit their live streams and say hello. You can also join VTuber communities on Discord, Reddit, and Facebook.`,
         fame: 100,
     },
     dryRun: {
         text: `Set up Your Gear, Test, and Practice`,
-        image: `./Game_Assets/dryrun.png`,
+        image: `./Assets/dryrun.png`,
         explainer: `To make your debut as smooth and stress-free as possible, set up all of your gear and do test recordings. VTubers predominantly need to rely on tech, so ensure everything is working as it should before you go live. You can set up all of your scenes and sources ahead of time in most streaming software, including any VTuber software you'll use. If you've never streamed before, you can practice by recording yourself, watching the footage, and checking what you need to improve. As with everything in this world, practice makes progress!`,
         fame: 100,
     },
@@ -118,7 +118,9 @@ function createMascotPrompts() {
 function question() {
     document.body.style.backgroundImage = '';
     if(currentQ == 1){
-        alert(`Your VTuber debut is in 8 Days! Each day, you can choose what to work on to make it a roaring success.<br><br> A VTuber Debut is simply when you stream with your VTuber for the first time. While having an official debut is optional, some VTubers like to amass followers on social media platforms before their debut to have a grander audience. Although on average, VTubers plan their debuts between a few weeks to a couple of months ahead.`);
+        alert(`Your VTuber debut is in 8 Days! Each day, you can choose what to work on to make it a roaring success.
+        
+        A VTuber Debut is simply when you stream with your VTuber for the first time. While having an official debut is optional, some VTubers like to amass followers on social media platforms before their debut to have a grander audience. Although on average, VTubers plan their debuts between a few weeks to a couple of months ahead.`);
     } else if (currentQ == 9) {
         alert(`You did it!`)
         playAgain();
@@ -165,7 +167,7 @@ function startGame() {
         // The game will end.
         document.getElementById('text').innerHTML = `You decide not to become a VTuber and live out the rest of your life as a really boring human being. The End.`
         document.getElementById(`option-buttons`).innerHTML = '';
-        document.body.style.backgroundImage = `url('./Game_Assets/boringlife.jpg')`;
+        document.body.style.backgroundImage = `url('./Assets/boringlife.jpg')`;
         return
     } 
 }
